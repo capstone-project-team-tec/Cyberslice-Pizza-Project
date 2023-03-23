e608ac66772812aebefe88ccfe042881ef0a74e3
tylercausey <tylercausey1219@gmail.com>
2023-03-23 11:08:44 -0500

    added a users file in db and a couple of user functions plus boilerplate

--- db/index.js
+++ db/index.js
@@ -1,22 +1,13 @@
 //This is the /db index.js
+const {client} = require('pg')
+require('dotenv').config()
+const client = new Client('http://localhost3000')
+
 
 
 
 //Users
-async function createUser({username, password}) {
-    try {
-        const {rows} = await client.query(`
-        INSERT INTO users(username, password)
-        VALUES ($1,$2)
-        ON CONFLICT (username) DO NOTHING
-        RETURNING  *;
-        ` [username, password])
 
-        return rows
-    } catch(error) {
-        console.log(error)
-    }
-}
 
 
 //Pizza
@@ -25,19 +16,49 @@ async function createUser({username, password}) {
 //Toppings
 
 
-//Drinks
+client.query(`
+    CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        address VARCHAR(255),
+        phone INTEGER UNIQUE        
+    );
+    CREATE TABLE products (
+        id SERIAL PRIMARY KEY,
+        category VARCHAR(255) NOT NULL,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        price FLOAT NOT NULL 
+    ); 
+    CREATE TABLE carts (
+        id SERIAL PRIMARY KEY,
+        "userId" INTEGER REFERENCES users(id),
+        "isCheckedOut" BOOLEAN DEFAULT FALSE
+    );
+    CREATE TABLE orderItems(
+        id SERIAL PRIMARY KEY,
+        "productId" INTEGER REFERENCES products(id),
+        "cartId" INTEGER REFERNCES carts(id), 
 
-
-//Sides
-
-
-//Desserts
-
-
-//Cart
-
-
-//Order Items
+    );
+    CREATE TABLE pizza(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        "pizzaCost" FLOAT NOT NULL
+    );
+    CREATE TABLE toppings (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255),
+        price FLOAT NOT NULL
+    );
+    CREATE TABLE pizzaWithToppings (
+        id SERIAL PRIMARY KEY
+        "pizzaID" REFERENCES pizza(id),
+        "toppingsId" REFERENCES toppings(id),
+        count INTEGER NOT NULL
+    );
+`)
 
 
 
---
+++ db/users.js
@@ -0,0 +1,110 @@
+const {client} = require("./client");
+const bcrypt = require("bcrypt");
+
+async function createUser({username, password, email, address, phone}) {
+    try {
+        const {rows} = await client.query(`
+        INSERT INTO users(username, password, email, address, phone)
+        VALUES ($1,$2, $3, $4, $5)
+        ON CONFLICT (username, phone, email) DO NOTHING
+        RETURNING  *;
+        ` [username, password, email, address, phone])
+
+        return rows
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+async function getUser({ username, password }) {
+    try {
+        const { rows : [user] } = await client.query(`
+          SELECT * 
+          FROM users
+          WHERE username = $1;
+        `, [username]);
+    
+        if (rows.length === 0) {
+          throw new Error('User not found');
+        }
+
+        const hashedPassword = user.password;
+    
+        const isValidPassword = await bcrypt.compare(password, hashedPassword);
+        if (!isValidPassword) {
+          throw new Error('Invalid password');
+        }
+
+        const hashedEmail = user.email;
+    
+        const isValidEmail = await bcrypt.compare(email, hashedEmail);
+        if (!isValidEmail) {
+          throw new Error('Invalid email');
+        }
+
+        const hashedAddress = user.address;
+    
+        const isValidAddress = await bcrypt.compare(address, hashedAddress);
+        if (!isValidAddress) {
+          throw new Error('Invalid address');
+        }
+    
+        const hashedPhone = user.phone;
+    
+        const isValidPhone = await bcrypt.compare(phone, hashedPhone);
+        if (!isValidPhone) {
+          throw new Error('Invalid phone number');
+        }
+
+        return user;
+      } catch (error) {
+        throw error;
+      }
+}
+
+async function getUserById(userId) {
+    try {
+
+        const {rows: [user] } = await client.query(`
+            SELECT id, username FROM users
+            WHERE id=$1;
+        `,[userId]);
+
+        if (!user) {
+            return null
+        }
+
+        return user;
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+async function getUserByUsername(userName) {
+    try {
+        const {rows} = await client.query(`
+            SELECT id, username, password 
+            FROM users
+            WHERE username=$1;
+        `, [userName])
+
+        // console.log(rows);
+        if(rows){
+            return rows[0];
+        }
+        else{
+            return undefined;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    createUser,
+    // getUser,
+    // getUserById,
+    // getUserByUsername,
+  }
