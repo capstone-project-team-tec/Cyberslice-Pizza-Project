279274f3df45d81dd13c7e47dacf6304b70039ca
tylercausey <tylercausey1219@gmail.com>
2023-03-31 17:19:05 -0500

    fixed tyler password for database

--- db/client.js
+++ db/client.js
@@ -1,4 +1,6 @@
 const { Pool, Client } = require('pg');
+require('dotenv').config()
+const { TYLER_DATABASE_PASSWORD } = process.env
 
 // Connects to the local postgresql database.
 const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/cyberslicepizza';
@@ -8,7 +10,9 @@ const client = new Client(connectionString);
 //   connectionString
 // //   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 // });
-
+if (TYLER_DATABASE_PASSWORD){
+    client.password = TYLER_DATABASE_PASSWORD
+}
 module.exports = {
     client
 }
--- db/index.js
+++ db/index.js
@@ -1,8 +1,5 @@
 //This is the /db index.js
-const {Client} = require('pg')
-require('dotenv').config()
-const client = new Client('http://localhost3000')
-client.password = 'alpaca'
+
 
 
 
