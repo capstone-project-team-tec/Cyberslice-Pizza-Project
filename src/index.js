dd7d4aee19de5828e593ce3253fa0e8174a8d962
Charlie Jenkins <charliedj13@gmail.com>
2023-04-01 14:01:20 -0500

    render

--- src/index.js
+++ src/index.js
@@ -1,9 +1,88 @@
 //This is the Frontend index.js
 import { useEffect, useState } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRoute, Route, Routes, Link } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Link, } from "react-router-dom"
+import { Checkout, Desserts, Drinks, Home, Locations, Login, OrderOptions, Pizza, Sides } from "./components"
 
 const App = () => {
+    const [drinks, setDrinks] = useState([])
+    const [desserts, setDesserts] = useState([])
+    const [sides, setSides] = useState([])
+    const {currentUser, setCurrentUser} = useState({})
+
+    async function fetchDrinks() {
+        try {
+            const response = await fetch(`https://localhost:1337/api/drinks`);
+
+            const data = await response.json();
+
+            setDrinks(data)
+        } catch(error) {
+            console.log(error)
+        }
+    }
+    
+    async function fetchDesserts() {
+        try {
+            const response = await fetch(`https://localhost:1337/api/desserts`);
+
+            const data = await response.json()
+
+            setDesserts(data)
+
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    async function fetchSides() {
+        try {
+            const response = await fetchDesserts(`https://localhost:1337/api/sides`);
+
+            const data = await response.json();
+
+            setSides(data)
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    async function fetchCurrentUser() {
+        if (localStorage.token) {
+
+        try {
+            const response = await fetch(`https://localhost:1337/api/users` , {
+            headers: {
+                'Content-Type': 'appplication/json',
+                'Authorization': `Bearer ${localStorage.token}`
+            },
+        });
+            
+           const data = await response.json();
+           
+           setCurrentUser(data)
+
+        } catch(error) {
+            console.log(error)
+        }
+    }
+    else {
+        setCurrentUser("")
+    }
+}
+
+useEffect(()=> {
+    fetchDesserts();
+    fetchDrinks();
+    fetchSides();
+    fetchCurrentUser();
+}, [])
+
+console.log(desserts, drinks, sides, currentUser)
+
+
+
+
     return (
 
         <BrowserRouter>
@@ -11,10 +90,10 @@ const App = () => {
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/pizza" element={<Pizza />}/>
-            <Route path="/drinks" element={<Drinks />}/>
-            <Route path="/sides" element={<Sides />} />
-            <Route path="/desserts" element={<Desserts />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/drinks" element={<Drinks drinks={drinks}/>}/>
+            <Route path="/sides" element={<Sides sides={sides}/>} />
+            <Route path="/desserts" element={<Desserts desserts={desserts}/>} />
+            <Route path="/login" element={<Login setCurrentUser={setCurrentUser}/>} />
             <Route path="/orderoptions" element={<OrderOptions />} />
             <Route path="/locations" element={<Locations />} />
             <Route path="/checkout" element={<Checkout />} />
