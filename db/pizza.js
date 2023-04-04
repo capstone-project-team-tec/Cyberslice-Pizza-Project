ce0a92fb61b1f14717873317d7d7968d2d47abe3
tylercausey <tylercausey1219@gmail.com>
2023-04-03 19:42:24 -0500

    created db pizza file, created a few db pizza functions including a few that interact with the pizzaWithToppings table, updated api index file with the new pizza route, and set up a few routes in the api pizza file

---
+++ db/pizza.js
@@ -0,0 +1,75 @@
+const {client} = require("./client");
+
+// pizza with toppings table functions
+async function addToppingsToPizza({pizzaId, toppingsId, count}) {
+    try {
+        const {rows} = await client.query(`
+        INSERT INTO "pizzaWithToppings"("pizzaId", "toppingsId", count)
+        VALUES ($1, $2, $3)
+        RETURNING  *;
+        `, [pizzaId, toppingsId, count]);
+
+        return rows
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+async function fetchPizzaWithToppingsInfo({pizzaId}) {
+    try {
+        const { rows : [user] } = await client.query(`
+            SELECT *
+            FROM pizzaWithToppings
+            WHERE pizzaID = $1;
+        `, [pizzaId])
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+// pizza table functions
+async function createPizza() {
+    try {
+        const { rows } = await client.query(`
+            INSERT INTO pizza (name, "basePizzaCost", size)
+            VALUES (NULL, NULL, NULL)
+            RETURNING *;
+        `);
+        return rows[0];
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+async function addDetailsToPizza(id, {name, basePizzaCost, size}) {
+    try {
+        const { rows } = await client.query(`
+            UPDATE pizza
+            SET name = $2, "basePizzaCost" = $3, size = $4
+            WHERE id = $1;
+        `, [id, name, basePizzaCost, size]);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+async function fetchPizzaByName(name) {
+    try {
+        const {rows: [pizza] } = await client.query(`
+        SELECT * FROM pizza
+        WHERE name=$1;
+        `, [name])
+        return pizza
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    addToppingsToPizza,
+    fetchPizzaWithToppingsInfo,
+    createPizza,
+    addDetailsToPizza,
+    fetchPizzaByName
+  }
