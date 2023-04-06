19d64415c107115ac2efeb3d35a24b5ee99b759b
NobleDot <ehernandez3122@gmail.com>
2023-04-06 15:27:15 -0500

    Checkout/Cart front-end appearance layout!

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -41,52 +41,55 @@ const Checkout = (props) => {
             <section id = "checkoutPageTitle">My Cart</section>
             
             {currentOrderItems.length > 0 ? (
-        currentOrderItems.map((orderItem) => {
-          return (
-            <div key={orderItem.id}>
-                <h2>ProductName: {orderItem.productName}</h2>
-                <h2>PizzaName: {orderItem.pizzaName}</h2>
-                <h2> Price: {orderItem.cost}</h2>
-                <h2> ProductId: {orderItem.productId}</h2>
-                <h2> PizzaId: {orderItem.pizzaId}</h2>
-                <h2> Count: {orderItem.count}</h2>
-                
-            </div>
-          );
-        })
-      ) : (
-        <div>Nothing in the cart yet!</div>
-      )}
+                currentOrderItems.map((orderItem) => {
+                return (
+                    <section className = "item"> 
+                        <div key={orderItem.id}>
+                            {/* only one of these should print */}
+                            <section className = "itemTitle">{orderItem.productName}</section>
+                            <section className = "itemTitle">{orderItem.pizzaName}</section>
 
-        <section id="subtotal">
-            <section id="subtotalTitle">Subtotal</section>
-            <section className="price">{subTotalCost.toFixed(2)}</section>
-        </section>
-            {/* <section id = "itemsList">
-                itemsList
-                <section className = "item">
-                    <section className = "itemTitle">
-                        itemTitle
-                    </section>
+                            <section className = "itemDetailsContainer">
 
-                    <section className = "itemDetailsContainer">
+                                {/* Space these better, away from the remove. */}
+                                <section className = "detailColumn">
+                                    <section className = "detailCategory">
+                                        Quantity
+                                    </section>
 
-                        <section className = "detailColumn">
-                            <section className = "detailCategory"> 
-                                Category
+                                    <section className = "detailValue">
+                                        {orderItem.count}
+                                    </section>
+                                </section>
+
+                                <section className = "detailColumn">
+                                    <section className = "detailCategory">
+                                        Total
+                                    </section>
+
+                                    <section className = "detailValue">
+                                        {orderItem.cost}
+                                    </section>
+                                </section>
+
+                                {/* This needs to go to the far right */}
+                                <section className = "removeButtonContainer">
+                                    <section className = "removeButton"> Remove </section>
+                                </section>
+                                
                             </section>
-
-                            <section className = "detailValue"> 
-                                Value
-                            </section>
-                        </section>
-
-                        <section className = "removeButton">
-                            Remove
-                        </section>
+                            
+                            {/* <h2> Price: {orderItem.cost}</h2>
+                            <h2> ProductId: {orderItem.productId}</h2>
+                            <h2> PizzaId: {orderItem.pizzaId}</h2>
+                            <h2> Count: {orderItem.count}</h2> */}
+                        </div>
                     </section>
-                </section>
-            </section>
+                );
+                })
+            ) : (
+                <div>Nothing in the cart yet!</div>
+            )}
 
             <section id = "totalContainer">
                 <section className = "chargeContainer">
@@ -94,32 +97,95 @@ const Checkout = (props) => {
                         <section id = "subtotalTitle">
                             Subtotal
                         </section>
+
                         <section className = "price">
-                            10.55
+                            $ {subTotalCost.toFixed(2)}
                         </section>
                     </section>
-                     */}
-                    {/* <section id = "fee">
+
+                    <section id = "fee">
                         <section id = "feeTitle">
-                            
                             Delivery Fee
                         </section>
                         <section className = "price">
-                            2.99
+                            $ 10.00
                         </section>
                     </section>
 
                     <section id = "total">
                         <section id = "totalTitle">
-                            Total:
+                            Total: 
                         </section>
 
                         <section className = "price">
-                            2.99
+                            
                         </section>
                     </section>
+                </section>
             </section>
-            </section> */}
+        {/* <section id = "itemsList">
+            itemsList
+            <section className = "item">
+                <section className = "itemTitle">
+                    itemTitle
+                </section>
+
+                ============= 
+
+
+                <section className = "itemDetailsContainer">
+
+                    <section className = "detailColumn">
+                        <section className = "detailCategory"> 
+                            Category
+                        </section>
+
+                        <section className = "detailValue"> 
+                            Value
+                        </section>
+                    </section>
+
+===================================
+
+                    <section className = "removeButton">
+                        Remove
+                    </section>
+                </section>
+            </section>
+        </section>
+
+        <section id = "totalContainer">
+            <section className = "chargeContainer">
+                <section id = "subtotal">
+                    <section id = "subtotalTitle">
+                        Subtotal
+                    </section>
+                    <section className = "price">
+                        10.55
+                    </section>
+                </section>
+                    */}
+                {/* <section id = "fee">
+                    <section id = "feeTitle">
+                        
+                        Delivery Fee
+                    </section>
+                    <section className = "price">
+                        2.99
+                    </section>
+                </section>
+
+                <section id = "total">
+                    <section id = "totalTitle">
+                        Total:
+                    </section>
+
+                    <section className = "price">
+                        2.99
+                    </section>
+                </section>
+        </section>
+        </section> */}
 {/* Depends on what the carryout/delivery choice was */}
             <section id = "buttonContainer"> 
                 <section id = "checkoutButton">Check out</section>
