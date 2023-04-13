bdc2357de11fd00d9f5511ee8d01bb5bf8bd9124
NobleDot <ehernandez3122@gmail.com>
2023-04-12 22:57:20 -0500

    Added comments!

--- api/admin.js
+++ api/admin.js
@@ -3,6 +3,7 @@ const { requireAdmin } = require("./utils");
 const adminRouter = express.Router()
 require("dotenv").config()
 
+// Database function wrappers
 const { 
     createProduct,
     fetchAllProducts,
@@ -17,11 +18,13 @@ const {
     deleteUser,
 } = require('../db/admin');
 
+// Logs a message for every request made.
 adminRouter.use((req,res,next)=>{
     console.log("A request is being made to /admin");
     next();
 })
 
+// GET all the products.
 adminRouter.get('/products', async(req,res,next)=>{
     try {
         const products = await fetchAllProducts();
@@ -31,6 +34,7 @@ adminRouter.get('/products', async(req,res,next)=>{
       }
 })
 
+// GET all users.
 adminRouter.get('/users', async(req,res,next)=>{
   console.log("get all users request is running")
   try {
@@ -41,6 +45,7 @@ adminRouter.get('/users', async(req,res,next)=>{
     }
 })
 
+// GET a user by their ID via the URL's path.
 adminRouter.get('/:userId', async (req, res, next) => {
   if (!req.params.userId) {
     console.log(error);
@@ -56,6 +61,7 @@ adminRouter.get('/:userId', async (req, res, next) => {
   }
 });
 
+// PATCH a product through its ID.
 adminRouter.patch('/products/:productId', requireAdmin, async (req, res, next) => {
   const id = req.params.productId;
   console.log("productsRouter.patch; productId: " + id);
@@ -88,135 +94,136 @@ adminRouter.patch('/products/:productId', requireAdmin, async (req, res, next) =
     next({ name, message });
   }
 });
+
+// GET a product by its ID.
+adminRouter.get('/product/:productId', async (req, res, next) => {
+  if (!req.params.productId) {
+    console.log(error);
+    next(error);
+  }
+  const product = await getProductById(req.params.productId);
+  try {
+    res.send(product);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+// POST a new product.
+adminRouter.post('/createproduct', requireAdmin, async(req,res,next)=> {
+  console.log("The create product route is running")
+  try {
+  const productData = req.body
+
+  if (productData.name && productData.price && (productData.category == 'desserts' || productData.category == 'drinks' || productData.category == 'sides')) {
+    let newCreatedProduct = await createProduct({
+      name: productData.name,
+      category: productData.category,
+      price: productData.price
+    })
+    if (newCreatedProduct) {
+      console.log("This is the new created product...",newCreatedProduct);
+      res.send(
+        {
+            success: true,
+            error: null,
+            message: "Product was successfully created."
+        }).status(200)
+    } else {
+      res.send({
+        success: false,
+        error: {
+          name: "UserError",
+          message: "failed to create account"
+        },
+        data: null
+      }).status(403)
+    }
+  }
   
-  adminRouter.get('/product/:productId', async (req, res, next) => {
-    if (!req.params.productId) {
-      console.log(error);
-      next(error);
-    }
-    const product = await getProductById(req.params.productId);
-    try {
-      res.send(product);
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
-  });
-
-  adminRouter.post('/createproduct', requireAdmin, async(req,res,next)=> {
-    console.log("The create product route is running")
-    try {
-    const productData = req.body
-
-    if (productData.name && productData.price && (productData.category == 'desserts' || productData.category == 'drinks' || productData.category == 'sides')) {
-      let newCreatedProduct = await createProduct({
-        name: productData.name,
-        category: productData.category,
-        price: productData.price
-      })
-      if (newCreatedProduct) {
-        console.log("This is the new created product...",newCreatedProduct);
-        res.send(
-          {
-              success: true,
-              error: null,
-              message: "Product was successfully created."
-          }).status(200)
-      } else {
-        res.send(
-            {
-                success: false,
-                error: {
-                    name: "UserError",
-                    message: "failed to create account"
-                },
-                data: null
-            }).status(403)
-      }
-    }
-    
-    } catch(error) {
-      console.log(error)
-    }
-  })
-
-  adminRouter.delete('/products/:productId', requireAdmin, async (req, res, next) => {
-    if (!req.params.productId) {
-      console.log(error);
-      next(error);
-    }
-    try {
-      const deletedProduct = await deleteProduct(req.params.productId);
-      res.send(deletedProduct);
-    } catch ({name, message}) {
-      next({name, message});
-    }
-  });
-
-  adminRouter.patch(`/users/:id`, requireAdmin, async (req, res, next) => {
-    console.log("This is the admin router patch running")
-    console.log(req.user)
-
-    const { id } = req.params
-    const { username, name, email, address, phone} = req.body
-    const updateFields = {}
-    if(id) {
-        updateFields.id = id
-    }
-    if (username) {
-        updateFields.username = username
-    }
-    if (name) {
-        updateFields.name = name
-    }
-    if (email) {
-        updateFields.email = email
-    }
-    if (address) {
-        updateFields.address = address
-    }
-    if (phone) {
-        updateFields.phone = phone
-    }
-    
-    try {
-      if (!id) {
-        res.send({
-              name: 'UserNotFoundError',
-              message: 'Could not find a user with that username'
-        })
-      } else {
-        const updatedUser = await updateUser(updateFields)
-        console.log(updatedUser)
-        res.send(updatedUser)
-      }
-    } catch(error) {
-        console.log(error)
-    }
+  } catch(error) {
+    console.log(error)
+  }
 })
 
-  adminRouter.delete('/users/:id', requireAdmin, async (req, res, next) => {
-    const {id} = req.params
-    try {
-    const id = await getUserByUsername(username);
-      if (!id) {
-          res.send( {
-              name: 'UserNotFoundError',
-              message: 'Could not find a user with that username'
-          } )
-      } else { 
-        await deleteUser(id)
-        res.send({
-            success: true,
-            message: "User was successfully deleted"
-        })
-      }
-    } catch ({ name, message }) {
-      next({ name, message });
-    }
-  });
+// DELETE a product by its ID.
+adminRouter.delete('/products/:productId', requireAdmin, async (req, res, next) => {
+  if (!req.params.productId) {
+    console.log(error);
+    next(error);
+  }
+  try {
+    const deletedProduct = await deleteProduct(req.params.productId);
+    res.send(deletedProduct);
+  } catch ({name, message}) {
+    next({name, message});
+  }
+});
 
+// PATCH a user by their ID.
+adminRouter.patch(`/users/:id`, requireAdmin, async (req, res, next) => {
+  console.log("This is the admin router patch running")
+  console.log(req.user)
+
+  const { id } = req.params
+  const { username, name, email, address, phone} = req.body
+  const updateFields = {}
+  if(id) {
+      updateFields.id = id
+  }
+  if (username) {
+      updateFields.username = username
+  }
+  if (name) {
+      updateFields.name = name
+  }
+  if (email) {
+      updateFields.email = email
+  }
+  if (address) {
+      updateFields.address = address
+  }
+  if (phone) {
+      updateFields.phone = phone
+  }
+  
+  try {
+    if (!id) {
+      res.send({
+            name: 'UserNotFoundError',
+            message: 'Could not find a user with that username'
+      })
+    } else {
+      const updatedUser = await updateUser(updateFields)
+      console.log(updatedUser)
+      res.send(updatedUser)
+    }
+  } catch(error) {
+      console.log(error)
+  }
+})
+
+// DELETE a user by their ID.
+adminRouter.delete('/users/:id', requireAdmin, async (req, res, next) => {
+  const {id} = req.params
+  try {
+  const id = await getUserByUsername(username);
+    if (!id) {
+        res.send( {
+            name: 'UserNotFoundError',
+            message: 'Could not find a user with that username'
+        } )
+    } else { 
+      await deleteUser(id)
+      res.send({
+          success: true,
+          message: "User was successfully deleted"
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 module.exports = adminRouter
-
-
